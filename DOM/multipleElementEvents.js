652938b369e06de9db336579ff8c86c822c94cf0
scurry222 <scurry222@gmail.com>
2020-03-24 13:19:55 -0700

    start of pairing swapped boxes function

--- DOM/multipleElementEvents.js
+++ DOM/multipleElementEvents.js
@@ -1,3 +1,30 @@
+
+
+// Example pairings: 
+// index: 1, 2, 3, 4, 5, 6
+//        ^  ^  ^  ^  ^  ^
+// pair:  1  2  3  4  5  6
+//        &  &  &  &  &  &
+//        2  1  5  2  3  3
+//           &  &
+//           4  6
+// index of self must be in pairing
+
+// colors[1] = [yellow, green]
+// colors[2] = [green, yellow, purple]
+// colors[3] = [blue, indigo, violet]
+// colors[4] = [purple, green]
+// colors[5] = [indigo, blue]
+// colors[6] = [violet, blue]
+
+// Simple Method
+// index: 1, 2, 3, 4, 5, 6
+//        ^  ^  ^  ^  ^  ^
+//        1  2  3  4  5  6
+//        &  &  &  &  &  &
+//        5  3  2  2  6  1
+//        &  &  &  &  &  &
+//        6  4  4  3  1  5
 colors = [
     'yellow',
     'green',
@@ -6,6 +33,28 @@ colors = [
     'indigo',
     'violet'
 ];
+const pairs = [];
+
+const createPairs = (colors) => {
+    for (let i = 0; i < colors.length - 1; i++) {
+        pairs.push(colors[i])
+        pairs[i] = [];
+        console.log(pairs[i]);
+        for (let j = i; j < i + 2; j++) {
+            pairs[i].push(colors[j]);
+            console.log(pairs[i]);
+
+        }
+
+    }
+}
+
+colors.forEach(function(color, i) {
+    const amountOfPairs = Math.floor(Math.random() * colors.length);
+    for (let j = 0; j < amountOfPairs; j++) {
+
+    }
+});
 
 const pickRandomColor = function() {
     let rand = colors[Math.floor(Math.random() * colors.length)];
