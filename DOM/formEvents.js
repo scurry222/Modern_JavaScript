fb40b9da545b7154bf9a16415edc19a26051f760
scurry222 <scurry222@gmail.com>
2020-03-24 13:20:37 -0700

    form events

---
+++ DOM/formEvents.js
@@ -0,0 +1,22 @@
+const form = document.querySelector('#signup-form');
+
+const creditCardInput = document.querySelector('#cc');
+const termsCheckbox = document.querySelector('#terms');
+const veggieSelect = document.querySelector('#veggie');
+
+form.addEventListener('submit', function(e) {
+	e.preventDefault(); //stops the request from being sent (prevents page reload)
+	console.log('cc', creditCardInput.value);
+	console.log('terms', termsCheckbox.checked);
+	console.log('veggieSelect', veggieSelect.value);
+	//send form data to db
+	//append something to page using form data
+});
+
+const formData = {};
+for (let input of [creditCardInput, termsInput, selectVeggieInput]) {
+	input.addEventListener('change', ({target}) => {
+		const {name, type, value, checked} = target;
+		formData[name] = type === checkbox ? checked : value;
+	})
+}
